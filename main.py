87f9821c092f4a9dc216703c5eca9c0188487572
Dmitriy <dima.verin.2002@mail.ru>
2021-12-13 15:55:23 +0300

    modified

--- main.py
+++ main.py
@@ -24,12 +24,35 @@ dr_car_park = [
     DrCarPark(3,3),
     DrCarPark(2,4),
     DrCarPark(3,5),
- 
+    
     DrCarPark(11,1),
     DrCarPark(22,2),
     DrCarPark(33,3),
     DrCarPark(22,4),
     DrCarPark(33,5)]
+
+def first_task(car_parks, one_to_many):
+    res_11 = [(c_p.name,
+            list(fio for fio, _, name in one_to_many if name == c_p.name))
+            for c_p in car_parks if c_p.name[0] == 'А']
+    return res_11 
+
+def second_task(car_parks, one_to_many):        
+    res_12_unsorted = []
+    for c_p in car_parks:
+        c_p_drivers = list(filter(lambda x: x[2] == c_p.name, one_to_many))
+        if len(c_p_drivers) > 0:
+            res_12_unsorted.append(
+                (c_p.name, max(c_p_drivers, key = lambda x: x[1])[1]))
+    res_12 = sorted(res_12_unsorted, key = itemgetter(1), reverse = True)
+    return res_12
+ 
+def third_task(many_to_many):
+    res_13 = []
+    for fio, _, c_p_name in many_to_many:
+        res_13.append((fio, c_p_name))
+    res_13 = sorted(res_13, key = itemgetter(1))
+    return res_13
  
 def main():
     one_to_many = [(dr.fio, dr.sal, c_p.name) 
@@ -43,32 +66,15 @@ def main():
         if c_p.id == d_c_p.car_park_id]
     
     many_to_many = [(dr.fio, dr.sal, c_p_name) 
-        for c_p_name, car_park_id, driver_id in many_to_many_temp
+        for c_p_name, _, driver_id in many_to_many_temp
         for dr in drivers if dr.id == driver_id]
- 
+        
     print('Задание Г1')
-    res_11 = [(c_p.name,
-              list(fio for fio, _, name in one_to_many if name == c_p.name))
-              for c_p in car_parks if c_p.name[0] == 'А']
-    print(res_11) 
-    
-    print('\nЗадание Г2')
-    res_12_unsorted = []
-    for c_p in car_parks:
-        c_p_drivers = list(filter(lambda x: x[2] == c_p.name, one_to_many))
-        if len(c_p_drivers) > 0:
-            res_12_unsorted.append(
-                (c_p.name, max(c_p_drivers, key = lambda x: x[1])[1]))
- 
-    res_12 = sorted(res_12_unsorted, key = itemgetter(1), reverse = True)
-    print(res_12)
- 
-    print('\nЗадание Г3')
-    res_13 = []
-    for fio, _, c_p_name in many_to_many:
-        res_13.append((fio, c_p_name))
-    res_13 = sorted(res_13, key = itemgetter(1))
-    print(res_13)
- 
+    print(first_task(car_parks, one_to_many))
+    print('Задание Г2')
+    print(second_task(car_parks, one_to_many))
+    print('Задание Г3')
+    print(third_task(many_to_many))
+
 if __name__ == '__main__':
     main()
